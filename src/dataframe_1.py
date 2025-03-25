5ed4e6a8ef1ce720b326931b82452f0f70690e43
Test01 <test01@gitlab.local>
2025-03-25 20:03:24 +0800

    250325

--- src/dataframe_1.py
+++ src/dataframe_1.py
@@ -1,3 +1,21 @@
 import pandas as pd
 url = "https://od.cdc.gov.tw/eic/NHI_EnteroviralInfection.csv"
 df = pd.read_csv(url)
+
+# df.iloc[4,4]
+# df.loc[4,"縣市"]
+
+# df.iloc[2:6,]
+# df.loc[2:5,]
+
+# df.iloc[:,1:5]
+# df.loc[:,"週":"縣市"]
+
+# df["週"]
+# df["週":"縣市"] error
+
+# df[["週","縣市"]]
+# df[["週","就診類別","年齡別","縣市"]]
+
+# type(df["週"])
+# type(df[["週","縣市"]])

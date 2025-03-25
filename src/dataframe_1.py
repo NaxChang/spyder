ae91cc4f28435e958a65b06c8c0e6fbcb6742b08
Test01 <test01@gitlab.local>
2025-03-25 20:01:04 +0800

    250325

---
+++ src/dataframe_1.py
@@ -0,0 +1,3 @@
+import pandas as pd
+url = "https://od.cdc.gov.tw/eic/NHI_EnteroviralInfection.csv"
+df = pd.read_csv(url)
--- src/plot1.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-listx = [1, 3, 5, 7, 9]
-listy = [0, 2, 4, 6, 10]
-
-plt.plot(listx, listy)
-plt.savefig("plot.png")
-plt.show()
